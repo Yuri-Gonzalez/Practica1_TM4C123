ba80f037878ad15f22975e94f5ded8768b52599d
Yuri-Gonzalez <ygonzalez27@alumnos.uaq.mx>
2022-03-16 01:09:04 -0600

    Entrar

---
+++ gcc/startup_gcc.d
@@ -0,0 +1,9 @@
+gcc/startup_gcc.o: startup_gcc.c \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\aplicaciones\ extra\gcc-arm\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ lib/hw_nvic.h lib/hw_types.h
